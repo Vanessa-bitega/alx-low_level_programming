302824149840f73950b93264b51c35c524dd98bc
root <vanessabewe@gmail.com>
2023-02-23 13:15:30 -0800

    print from 0 to 9

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * print_numbers - print numbers from 0 to 9
+ * Return: 0
+ */
+void print_numbers(void)
+{
+	int v;
+
+	for (v = 0; v <= 0; v++)
+	{
+		putchar(v + '0');
+	}
+	putchar('\n');
+}
