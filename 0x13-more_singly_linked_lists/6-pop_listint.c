054cd105dfac24b3e26910a8973a3e4bc32f5259
root <vanessabewe@gmail.com>
2023-04-04 05:46:15 -0700

    Write a function that deletes the head node of a listint_t linked list, and returns the head node’s data (n).
    Prototype: int pop_listint(listint_t **head);
    if the linked list is empty return 0

---
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -0,0 +1,21 @@
+/**
+ * pop_listint - deletes the head node of a listint_t linked list
+ * @head: pointer to pointer to the head node of the list
+ *
+ * Return: the head node's data (n), or 0 if the list is empty
+ */
+int pop_listint(listint_t **head)
+{
+	int data;
+
+	listint_t *temp;
+
+	if (*head == NULL)
+		return (0);
+	data = (*head)->n;
+	temp = *head;
+	*head = (*head)->next;
+	free(temp);
+
+	return (data);
+}
