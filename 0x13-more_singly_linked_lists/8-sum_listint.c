1c62f8ca9de141b2a0eb7ce87044148f4fe87269
root <vanessabewe@gmail.com>
2023-04-04 07:19:14 -0700

    fixed betty errors

--- 0x13-more_singly_linked_lists/8-sum_listint.c
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -1,20 +1,19 @@
 #include "lists.h"
 /**
- * sum_listint - Computes the sum of all the data (n) of a listint_t linked list.
+ * sum_listint - Computes the sum of all the data (n) of a listint_t
  * @head: Pointer to the first node of the list.
  *
- * Return: The sum of all the data (n) of the list. If the list is empty, returns 0.
+ * Return: The sum of all the data (n) of the list. If the list is empty,
  */
 int sum_listint(listint_t *head)
 {
-    int sum = 0;
-    listint_t *current = head;
+	int sum = 0;
+	listint_t *current = head;
 
-    while (current)
-    {
-        sum += current->n;
-        current = current->next;
-    }
-
-    return sum;
+	while (current)
+	{
+		sum += current->n;
+		current = current->next;
+	}
+	return (sum);
 }
